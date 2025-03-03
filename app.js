bc2e208524147cc260b6301bf852e72eeae5e439
Daniel Emerson <91567321+eme20003@users.noreply.github.com>
2025-03-03 13:38:20 -0500

    Recent adjustments Auth0

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+// Initialize express app
+const app = express();
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./backend/db/connect');
 require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
 
-// Initialize express app
-const app = express();
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 
 // Now you can call setupSwagger
 const setupSwagger = require('./swagger');
@@ -16,7 +35,7 @@ const port = process.env.PORT || 8080;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/users', require('./backend/routes/users'));
+app.use('/api/users', requiresAuth(), require('./backend/routes/users'));
 
 // Connect to Database and Start Server
 db.initDb((err) => {
@@ -27,6 +46,3 @@ db.initDb((err) => {
     app.listen(port, () => console.log(`Server running on port ${port}`));
   }
 });
-
-
-//THISS SUCKS
